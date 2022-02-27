a4c42acfbb6814321871cafa5d8967422dcc8ba6
Elke Dick <elke.codes@gmail.com>
2022-02-26 22:24:42 -0800

    header link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import Hamburger from "../Hamburger/Hamburger";
 import Logo from "../Logo/Logo";
 import "./Header.scss";
 
 const Header = () => {
+	const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
 	return (
 		<header className="header">
 			<Logo />
 
 			<nav className="header__nav">
-				<Link to="/" className="header__nav-link">
+				{/* <Hamburger setHamburgerOpen={setHamburgerOpen} />
+				{hamburgerOpen && (
+					<>
+						<NavLink
+							to="/gallery"
+							className="header__burger-nav-link"
+							activeClassName="burger-header__nav-link--active">
+							ETH Gallery
+						</NavLink>
+						<NavLink
+							to="/login"
+							className="header__burger-nav-link"
+							activeClassName="header__-burger-nav-link--active">
+							Login
+						</NavLink>
+					</>
+				)} */}
+				{/* <NavLink
+					to="/"
+					className="header__nav-link"
+					activeClassName="header__nav-link--active">
 					Home
-				</Link>
-				<Link to="/gallery" className="header__nav-link">
+				</NavLink> */}
+				<NavLink
+					to="/gallery"
+					className="header__nav-link"
+					activeClassName="header__nav-link--active">
 					ETH Gallery
-				</Link>
-				<Link to="/" className="header__nav-link">
+				</NavLink>
+				<NavLink
+					to="/login"
+					className="header__nav-link"
+					activeClassName="header__nav-link--active">
 					Login
-				</Link>
+				</NavLink>
 			</nav>
 		</header>
 	);

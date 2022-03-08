818668e4f573c391c9b3d6f19a0b4ae0e053a58f
Elke Dick <elke.codes@gmail.com>
2022-03-08 10:18:39 -0800

    nft card dropdown icon

--- src/components/NftCard/NftCard.jsx
+++ src/components/NftCard/NftCard.jsx
@@ -5,11 +5,9 @@ const NftCard = (props) => {
 	return (
 		<>
 			<li className="card">
-				{/* <a href=""> */}
 				<h2 className="card__collection-name">
 					{props.nft.name ? props.nft.name : null}
 				</h2>
-
 				<img src={props.nft.imageSrc} className="card__image" alt="" />
 				<div className="card__overlay">
 					<div className="card__header">
@@ -18,21 +16,13 @@ const NftCard = (props) => {
 							xmlns="http://www.w3.org/2000/svg">
 							<path />
 						</svg>
-						{/* <img
-													className="card__thumb"
-													src="https://i.imgur.com/7D7I6dI.png"
-													alt=""
-												/> */}
-
 						<div className="card__header-text">
 							<h3 className="card__title">
 								{props.nft.metadata && props.nft.metadata.name
 									? props.nft.metadata.name
 									: null}
+								<span> &#8964;</span>
 							</h3>
-							{/* <span className="card__status">
-														1 hour ago
-													</span> */}
 						</div>
 					</div>
 					<p className="card__description">
@@ -41,7 +31,6 @@ const NftCard = (props) => {
 							: "no description for this NFT"}
 					</p>
 				</div>
-				{/* </a> */}
 			</li>
 		</>
 	);

4986dbdc0c9f574ba8c2875992410a7faffd8aff
Elke Dick <elke.codes@gmail.com>
2022-03-11 12:07:10 -0800

    replace http with https

--- src/utils/transformMetadataUri.js
+++ src/utils/transformMetadataUri.js
@@ -1,10 +1,12 @@
 export const IPFS_GATEWAY_PREFIX = "https://ipfs.io/";
 export const transformMetadataUri = (metadataUri, id) => {
+	metadataUri = metadataUri.replace("http://", "https://");
 	if (!metadataUri.includes("ipfs")) {
 		metadataUri = metadataUri.replace("0x{id}", id);
 		metadataUri = metadataUri.replace("{id}", id);
 		return metadataUri;
 	}
+
 	metadataUri = metadataUri.replace("https://gateway.pinata.cloud/ipfs/", "");
 	metadataUri = metadataUri.replace("https://ipfs.moralis.io/", "");
 	metadataUri = metadataUri.replace("/metadata.json", "");
--- src/utils/transformMetadataUri.test.js
+++ src/utils/transformMetadataUri.test.js
@@ -94,3 +94,11 @@ it("should transform a http url that starts with https://gateway.pinata.cloud/",
 	const expected = IPFS_GATEWAY_PREFIX + "ipfs/" + "12345";
 	expect(transformed).toBe(expected);
 });
+
+it("should transform a http url that starts with http", () => {
+	const url = "http://gateway.pinata.cloud/ipfs/12345";
+
+	const transformed = transformMetadataUri(url);
+	const expected = IPFS_GATEWAY_PREFIX + "ipfs/" + "12345";
+	expect(transformed).toBe(expected);
+});

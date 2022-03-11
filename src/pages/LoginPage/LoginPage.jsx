e826052d7c8ee107e8efda0f34b1ecc5fb5e292b
Elke Dick <elke.codes@gmail.com>
2022-03-11 10:42:52 -0800

    add home button to login-page homepage text-align center

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,18 @@
 /// --- LOGINPAGE.JSX --- ///
 
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LoginPage.scss";
 
 const LoginPage = () => {
 	return (
-		<main>
+		<main className="login">
 			<h2 className="login__title">
 				Personalized options coming soon...
 			</h2>
+			<button className="login__button">
+				<Link to="/">back home</Link>
+			</button>
 		</main>
 	);
 };

882dba23c55fcd52b4ad110c52333f9cbf333eb7
Elke Dick <elke.codes@gmail.com>
2022-03-23 01:26:27 -0700

    undo last

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -19,9 +19,9 @@ const SearchBar = ({ onSearch, loadingNftsState, address }) => {
 				const provider = new ethers.providers.JsonRpcProvider(
 					INFURA_URL
 				);
-				const inputValue = e.target.searchInput.value.toLowerCase();
-				console.log("inputvalue", inputValue);
-				const addressFromEns = await provider.resolveName(inputValue);
+				const addressFromEns = await provider.resolveName(
+					e.target.searchInput.value
+				);
 				console.log("searchbar ens", addressFromEns);
 				onSearch(addressFromEns);
 			} catch (e) {
